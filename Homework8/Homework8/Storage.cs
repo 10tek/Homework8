fc0985d5158bc74615ae6e9c32ecf7002606399e
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-07-23 21:03:02 +0600

    Создал абстрактный базовый класс Storage

---
+++ Homework8/Homework8/Storage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Homework8
+{
+    abstract public class Storage
+    {
+        private string _carrierName;
+        private string _carrierModel;
+        public int GlobalMemory { get; set; }
+        public int FreeMemory { get; set; }
+        public int OccupiedMemory { get; set; }
+
+        public Storage(string carrierName, string carrierModel)
+        {
+            _carrierModel = carrierModel;
+            _carrierName = carrierName;
+        }
+
+        public abstract int CopyData(params int[] datas);
+        public abstract string GetInfo();
+    }
+}
